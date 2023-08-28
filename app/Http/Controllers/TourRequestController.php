1d37801b2e053a188b2360b62ab75e9dd6ffdba5
Hadi <hadikhanzadeh1992@gmail.com>
2023-08-28 10:06:51 +0330

    Complete tourRequest User Data

--- app/Http/Controllers/TourRequestController.php
+++ app/Http/Controllers/TourRequestController.php
@@ -80,9 +80,52 @@ class TourRequestController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoretourRequestRequest $request)
+    public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'company' => 'required|string',
+            'user_id' => 'required|integer',
+            'exhibition' => 'required|integer',
+            'exhibition-title' => 'required|string',
+            'country' => 'required|array',
+            'city' => 'required|array',
+            'genre' => 'required|array',
+            'phone' => 'required|string',
+            'email' => 'email',
+            'ceo-name' => 'required|string',
+            'participants' => 'required|integer'
+        ]);
+
+        if (!$validator->passes()) {
+            return json_encode(['status' => 'error', 'message' => 'اطلاعات ارسالی صحیح نمی باشد! لطفا اطلاعات خود به دقت بررسی نمایید.']);
+        }
+
+        try {
+            $tourRequest = new tourRequest;
+            $trackingCode = wbsUtility::randomInt(10);
+            $tourRequest->user_id = (int)$request->get('user_id');
+            $tourRequest->company_name = $request->get('company');
+            $tourRequest->exhibition_id = $request->get('exhibition');
+            $tourRequest->exhibition_title = $request->get('exhibition-title');
+            $tourRequest->country = $request->get('country')[0];
+            $tourRequest->country_title = $request->get('country')[1];
+            $tourRequest->city = $request->get('city')[0];
+            $tourRequest->city_title = $request->get('city')[1];
+            $tourRequest->activity_area = $request->get('genre')[0];
+            $tourRequest->activity_area_title = $request->get('genre')[1];
+            $tourRequest->mobile = $request->get('phone');
+            $tourRequest->participants = $request->get('participants');
+            $tourRequest->email = $request->get('email');
+            $tourRequest->manager = $request->get('ceo-name');
+            $tourRequest->tracking_code = $trackingCode;
+            $tourRequest->lang = $request->get('lang');
+
+            $tourRequest->save();
+            return json_encode(['status' => 'success', 'message' => 'درخواست شما با موفقیت ثبت گردید. کد پیگیری شما : ' . $trackingCode]);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+            return json_encode(['status' => 'error', 'message' => 'خطایی در پردازش اطلاعات ارسالی وجود دارد! لطفا با پشتیبانی مطرح نمایید.']);
+        }
     }
 
     /**
@@ -131,6 +174,7 @@ class TourRequestController extends Controller
             $tourRequest->email = $request->get('email');
             $tourRequest->manager = $request->get('manager');
             $tourRequest->status = $request->get('status');
+            $tourRequest->operator_id = \Auth::id();
 
             $tourRequest->save();
             $request->session()->flash('success', 'اطلاعات مورد نظر با موفقیت به روز رسانی گردید.');
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->post('/tourRequest', [\App\Http\Controllers\API\TourRequestController::class, 'store']);
+Route::middleware(['auth:sanctum'])->post('/tourRequest', [\App\Http\Controllers\TourRequestController::class, 'store']);
 
